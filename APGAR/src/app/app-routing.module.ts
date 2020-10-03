83c2e8fcd15ab2820e503bffdb54adb9576e5670
Pawinee Purachanai <pawinee.pu@outlook.com>
2020-10-03 13:08:56 +0700

    create all page

--- APGAR/src/app/app-routing.module.ts
+++ APGAR/src/app/app-routing.module.ts
@@ -10,7 +10,31 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'login',
+    loadChildren: () => import('./Auth/login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'register',
+    loadChildren: () => import('./Auth/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'video-tutorial',
+    loadChildren: () => import('./pages/video-tutorial/video-tutorial.module').then( m => m.VideoTutorialPageModule)
+  },
+  {
+    path: 'level1',
+    loadChildren: () => import('./pages/level1/level1.module').then( m => m.Level1PageModule)
+  },
+  {
+    path: 'level2',
+    loadChildren: () => import('./pages/level2/level2.module').then( m => m.Level2PageModule)
+  },
+  {
+    path: 'level3',
+    loadChildren: () => import('./pages/level3/level3.module').then( m => m.Level3PageModule)
+  },
+
 ];
 
 @NgModule({
---
+++ APGAR/src/app/pages/video-tutorial/video-tutorial.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video-tutorial',
+  templateUrl: './video-tutorial.page.html',
+  styleUrls: ['./video-tutorial.page.scss'],
+})
+export class VideoTutorialPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
